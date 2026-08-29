ffc117ab4567f79a3b7b668283cbfaea96c22276
Isaiah L Collins <Isaiah.lamar21@gmail.com>
2026-08-28 23:02:18 -0400

    refactor: simplify test procedure

--- engine/tests/test_bitboard.cpp
+++ engine/tests/test_bitboard.cpp
@@ -5,32 +5,6 @@
 bool testBoard() {
     int failed = 0;
 
-    Bitboard A1_board = squareToBitboard(Square::A1);
-    Bitboard A1_expected = 1ULL << 56;
-    if (A1_board != A1_expected) {
-        std::cout << "A1 failed. Expected: " << A1_expected << "\nReceived: " << A1_board << "\n";
-    }
-    Bitboard H1_board = squareToBitboard(Square::H1);
-    Bitboard H1_expected = 1ULL << 63;
-    if (H1_board != H1_expected) {
-        std::cout << "H1 failed. Expected: " << H1_expected << "\nReceived: " << H1_board << "\n";
-    }
-    Bitboard E4_board = squareToBitboard(Square::E4);
-    Bitboard E4_expected = 1ULL << 36;
-    if (E4_board != E4_expected) {
-        std::cout << "E4 failed. Expected: " << E4_expected << "\nReceived: " << E4_board << "\n";
-    }
-    Bitboard A8_board = squareToBitboard(Square::A8);
-    Bitboard A8_expected = 1ULL << 0;
-    if (A8_board != A8_expected) {
-        std::cout << "A8 failed. Expected: " << A8_expected << "\nReceived: " << A8_board << "\n";
-    }
-    Bitboard H8_board = squareToBitboard(Square::H8);
-    Bitboard H8_expected = 1ULL << 7;
-    if (H8_board != H8_expected) {
-        std::cout << "H8 failed. Expected: " << H8_expected << "\nReceived: " << H8_board << "\n";
-    }
-
     for (int i = 0; i < NUM_SQUARES; i++) {
         Square square = static_cast<Square>(i);
         std::cout << "Testing square: " << squareToFile(square) << squareToRank(square) << "\n";
@@ -55,35 +29,29 @@ bool testBoard() {
 bool testRank() {
     int failed = 0;
 
-    int A1_rank = squareToRank(Square::A1);
-    int A1_expected = 1;
-    if (A1_rank != A1_expected) {
+    if (squareToRank(Square::A1) != 1) {
         failed += 1;
-        std::cout << "A1 failed. Expected: " << A1_expected << "\nReceived: " << A1_rank << "\n";
+        std::cout << "A1 failed. Expected: " << 1 << "\nReceived: " << squareToRank(Square::A1) << "\n";
     }
-    int H1_rank = squareToRank(Square::H1);
-    int H1_expected = 1;
-    if (H1_rank != H1_expected) {
+
+    if (squareToRank(Square::H1) != 1) {
         failed += 1;
-        std::cout << "H1 failed. Expected: " << H1_expected << "\nReceived: " << H1_rank << "\n";
+        std::cout << "H1 failed. Expected: " << 1 << "\nReceived: " << squareToRank(Square::H1) << "\n";
     }
-    int E4_rank = squareToRank(Square::E4);
-    int E4_expected = 4;
-    if (E4_rank != E4_expected) {
+    
+    if (squareToRank(Square::E4) != 4) {
         failed += 1;
-        std::cout << "E4 failed. Expected: " << E4_expected << "\nReceived: " << E4_rank << "\n";
+        std::cout << "E4 failed. Expected: " << 4 << "\nReceived: " << squareToRank(Square::E4) << "\n";
     }
-    int A8_rank = squareToRank(Square::A8);
-    int A8_expected = 8;
-    if (A8_rank != A8_expected) {
+    
+    if (squareToRank(Square::A8) != 8) {
         failed += 1;
-        std::cout << "A8 failed. Expected: " << A8_expected << "\nReceived: " << A8_rank << "\n";
+        std::cout << "A8 failed. Expected: " << 8 << "\nReceived: " << squareToRank(Square::A8) << "\n";
     }
-    int H8_rank = squareToRank(Square::H8);
-    int H8_expected = 8;
-    if (H8_rank != H8_expected) {
+    
+    if (squareToRank(Square::H8) != 8) {
         failed += 1;
-        std::cout << "H8 failed. Expected: " << H8_expected << "\nReceived: " << H8_rank << "\n";
+        std::cout << "H8 failed. Expected: " << 8 << "\nReceived: " << squareToRank(Square::H8) << "\n";
     }
 
     if (failed == 0) {
@@ -96,35 +64,29 @@ bool testRank() {
 bool testFile() {
     int failed = 0;
 
-    char A1_file = squareToFile(Square::A1);
-    char A1_expected = 'A';
-    if (A1_file != A1_expected) {
+    if (squareToFile(Square::A1) != 'A') {
         failed += 1;
-        std::cout << "A1 failed. Expected: " << A1_expected << "\nReceived: " << A1_file << "\n";
+        std::cout << "A1 failed. Expected: 'A' \nReceived: " << squareToFile(Square::A1) << "\n";
     }
-    char H1_file = squareToFile(Square::H1);
-    char H1_expected = 'H';
-    if (H1_file != H1_expected) {
+    
+    if (squareToFile(Square::H1) != 'H') {
         failed += 1;
-        std::cout << "H1 failed. Expected: " << H1_expected << "\nReceived: " << H1_file << "\n";
+        std::cout << "H1 failed. Expected: 'H' \nReceived: " << squareToFile(Square::H1) << "\n";
     }
-    char E4_file = squareToFile(Square::E4);
-    char E4_expected = 'E';
-    if (E4_file != E4_expected) {
+    
+    if (squareToFile(Square::E4) != 'E') {
         failed += 1;
-        std::cout << "E4 failed. Expected: " << E4_expected << "\nReceived: " << E4_file << "\n";
+        std::cout << "E4 failed. Expected: 'E' \nReceived: " << squareToFile(Square::E4) << "\n";
     }
-    char A8_file = squareToFile(Square::A8);
-    char A8_expected = 'A';
-    if (A8_file != A8_expected) {
+    
+    if (squareToFile(Square::A8) != 'A') {
         failed += 1;
-        std::cout << "A8 failed. Expected: " << A8_expected << "\nReceived: " << A8_file << "\n";
+        std::cout << "A8 failed. Expected: 'A' \nReceived: " << squareToFile(Square::A8) << "\n";
     }
-    char H8_file = squareToFile(Square::H8);
-    char H8_expected = 'H';
-    if (H8_file != H8_expected) {
+    
+    if (squareToFile(Square::H8) != 'H') {
         failed += 1;
-        std::cout << "H8 failed. Expected: " << H8_expected << "\nReceived: " << H8_file << "\n";
+        std::cout << "H8 failed. Expected: 'H' \nReceived: " << squareToFile(Square::H8) << "\n";
     }
 
     if (failed == 0) {
